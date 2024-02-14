092e8c93cb8d51bb4dde893efeb4480e0c6e7452
a.molchansky <a.molchansky0@gmail.com>
2024-02-14 17:09:12 +0300

    require telegram bot package

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,7 +1,41 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"os"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
 
 func main() {
-	fmt.Println("Hello world!")
+	token := os.Getenv("TOKEN")
+
+	bot, errInit := tgbotapi.NewBotAPI(token)
+	if errInit != nil {
+		log.Panic(errInit)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Timeout: 60,
+	}
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You wrote:"+update.Message.Text)
+			msg.ReplyToMessageID = update.Message.MessageID
+
+			_, errSend := bot.Send(msg)
+			if errSend != nil {
+				log.Panic(errSend)
+			}
+		}
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/AMolchansky/demo_bot
 
 go 1.22.0
+
+require github.com/go-telegram-bot-api/telegram-bot-api/v5 v5.5.1
