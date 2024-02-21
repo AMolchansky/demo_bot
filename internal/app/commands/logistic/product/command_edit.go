4dfabab112074c7d1b4fb215248ba17f292f6b94
a.molchansky <a.molchansky0@gmail.com>
2024-02-21 18:05:21 +0300

    start implement edit command

--- internal/app/commands/logistic/product/command_edit.go
+++ internal/app/commands/logistic/product/command_edit.go
@@ -1,7 +1,30 @@
 package product
 
-import tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+import (
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"strconv"
+)
 
 func (pc *DummyProductCommander) Edit(inputMessage *tgbotapi.Message) {
+	args := inputMessage.CommandArguments()
 
+	productId, err := strconv.Atoi(args)
+	if err != nil {
+		pc.sendMessage(inputMessage.Chat.ID, "Invalid product id", "ProductCommander.Edit")
+		return
+	}
+
+	product, err := pc.dummyProductService.Describe(uint64(productId))
+	if err != nil {
+		pc.sendMessage(inputMessage.Chat.ID, "Product not found", "ProductCommander.Edit")
+		return
+	}
+
+	err = pc.dummyProductService.Update(uint64(productId), *product)
+	if err != nil {
+		pc.sendMessage(inputMessage.Chat.ID, "Failed to update product", "ProductCommander.Edit")
+		return
+	}
+
+	pc.sendMessage(inputMessage.Chat.ID, "Product updated successfully", "ProductCommander.Edit")
 }
